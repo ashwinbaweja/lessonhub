789091985d5966b57cc77176f9744a4d3c01ac6e
= <ashwinb10@gmail.com>
2014-03-30 01:56:51 -0400

    search works woohoo

--- lessonhub/__init__.py
+++ lessonhub/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, g, jsonify, Response, request, json, render_template, redirect, current_app
+import flask
 import pymongo
+
+# mongo
+client = pymongo.MongoClient()
+db = client['lessonhub']
 
 app = Flask(__name__)
 import lessonhub.routes
 import lessonhub.api
-# mongo
-client = pymongo.MongoClient()
-db = client['lessonhub']
 
 if __name__ == '__main__':
     app.run(debug=True)
